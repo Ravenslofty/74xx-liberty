6ddd5bb24a40af0c1d2607df26108f2f9b9ec2b8
Dan Ravensloft <dan.ravensloft@gmail.com>
2019-06-16 14:55:45 +0100

    Add IC counting script

---
+++ ic_count.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+import fileinput
+import math
+import re
+
+IC_LIST = dict()
+
+for line in fileinput.input():
+    match = re.search(r"^\s*\\(\d+)_(\d+)x(\d+)\S+\s*(\d+)", line)
+
+    if not match:
+        continue
+
+    part_number, gates_per_ic, gates_per_combo, combo_count = match.groups()
+
+    if part_number not in IC_LIST:
+        IC_LIST[part_number] = (
+                (int(combo_count) * int(gates_per_combo))
+                / int(gates_per_ic))
+    else:
+        IC_LIST[part_number] += (
+                (int(combo_count) * int(gates_per_combo))
+                / int(gates_per_ic))
+
+for part, count in IC_LIST.items():
+    count = math.ceil(count)
+    print("{}: {}".format(part, count))
