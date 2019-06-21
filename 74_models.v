dd9a7fc1ad5da2c96c2f9982b4a22d7cf703e319
Dan Ravensloft <dan.ravensloft@gmail.com>
2019-06-21 19:07:40 +0100

    Added BRAM rules and failed equality IC pass

--- 74_models.v
+++ 74_models.v
@@ -36,13 +36,22 @@ endmodule
 
 module \74AC377_8x1DFFE (D, CE, CP, Q);
 
-input D;
-input CE;
-input CP;
+input D, CE, CP;
 output reg Q;
 
 always @(posedge CP) begin
     if (!CE) Q <= D;
 end
+
+endmodule
+
+module \74HC688_1x1EQ8 (A, B, E, Q);
+
+input [7:0] A;
+input [7:0] B;
+input E;
+output Q;
+
+assign Q = E || !(A == B);
 
 endmodule
