755785df6ab206c2816f8673d014309b9e49b16f
Pepijn de Vos <pepijndevos@gmail.com>
2019-06-22 11:02:28 +0200

    add simulation makefile and testbenches
    Currently requires the bram pass to be commented out.
    Picorv32 runs but does not work correctly.
    TODO:
     * bram verilog model
     * find bug that breaks Pircorv32 testbench

---
+++ benchmarks/counter.v
@@ -0,0 +1,28 @@
+// Quick blinking a LED
+
+/* module */
+module counter (
+  input rst,
+  input clk,
+  output led
+    
+);
+
+    /* reg */
+    reg [7:0] counter = 0;
+    reg state;
+    
+    /* assign */
+    assign led = state;
+    
+    /* always */
+    always @ (posedge clk) begin
+      if (rst) begin
+        counter <= 0;
+      end else begin
+        counter <= counter + 1;
+        state <= counter[7];
+      end
+    end
+
+endmodule
---
+++ benchmarks/counter_tb.v
@@ -0,0 +1,22 @@
+module counter_tb ();
+ 
+  reg clk, rst;
+  wire led;
+ 
+  counter DUT (
+    .rst(rst),
+    .clk(clk),
+    .led(led)
+  );
+ 
+  initial begin
+    $dumpfile("counter.vcd");
+    $dumpvars(0,counter_tb);
+    clk = 1'b0;
+    rst = 1'b1;
+    repeat(4) #10 clk = ~clk;
+    rst = 1'b0;
+    repeat(1024) #10 clk = ~clk;
+  end
+ 
+endmodule
