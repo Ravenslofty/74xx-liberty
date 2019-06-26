45b89121ddcc8e7e93a5132981a3a93ed2206b49
Dan Ravensloft <dan.ravensloft@gmail.com>
2019-06-26 11:24:00 +0100

    7485/74688 conversion pass

--- 74_eq.v
+++ 74_eq.v
@@ -1,19 +1,22 @@
-(* techmap_celltype = "$add" *)
-module _80_74AC283_add (A, B, Y);
+// Note: this file is kept around for posterity; ABC is very good at
+// optimising equality checks.
+
+(* techmap_celltype = "$eq" *)
+module _80_74HC688_eq (A, B, Y);
 
 parameter A_SIGNED = 0;
 parameter B_SIGNED = 0;
-parameter A_WIDTH = 4;
-parameter B_WIDTH = 4;
-parameter Y_WIDTH = 4;
+parameter A_WIDTH = 0;
+parameter B_WIDTH = 0;
+parameter Y_WIDTH = 0;
 
 input [A_WIDTH-1:0] A;
 input [B_WIDTH-1:0] B;
 output [Y_WIDTH-1:0] Y;
 
-wire _TECHMAP_FAIL_ = Y_WIDTH <= 1;
+wire _TECHMAP_FAIL_ = A_WIDTH <= 6 && B_WIDTH <= 6;
 
-localparam WIDTH = ((Y_WIDTH + 3) / 4) * 4;
+localparam WIDTH = ((Y_WIDTH + 7) / 8) * 8;
 
 wire [Y_WIDTH-1:0] A_buf, B_buf;
 \$pos #(.A_SIGNED(A_SIGNED), .A_WIDTH(A_WIDTH), .Y_WIDTH(Y_WIDTH)) A_conv (.A(A), .Y(A_buf));
@@ -27,37 +30,36 @@ wire [WIDTH:0] C;
 assign C[0] = 0;
 
 genvar i;
-generate for (i = 0; i < WIDTH; i = i + 4) begin:slice
-    \74AC283_1x1ADD4 adder_i(
-        .A(AA[i+3:i]),
-        .B(BB[i+3:i]),
-        .CI(C[i]),
-        .S(YY[i+3:i]),
-        .CO(C[i+4]),
+generate for (i = 0; i < WIDTH; i = i + 8) begin:slice
+    \74HC688_1x1EQ8 eq_i (
+        .A(AA[i+7:i]),
+        .B(BB[i+7:i]),
+        .E(C[i]),
+        .Q(C[i+8])
     );
 end
 endgenerate
 
-assign Y = YY[Y_WIDTH-1:0];
+assign Y = !C[WIDTH];
 
 endmodule
 
-(* techmap_celltype = "$sub" *)
-module _80_74AC283_sub (A, B, Y);
+(* techmap_celltype = "$ne" *)
+module _80_74HC688_ne (A, B, Y);
 
 parameter A_SIGNED = 0;
 parameter B_SIGNED = 0;
-parameter A_WIDTH = 4;
-parameter B_WIDTH = 4;
-parameter Y_WIDTH = 4;
+parameter A_WIDTH = 0;
+parameter B_WIDTH = 0;
+parameter Y_WIDTH = 0;
 
 input [A_WIDTH-1:0] A;
 input [B_WIDTH-1:0] B;
 output [Y_WIDTH-1:0] Y;
 
-wire _TECHMAP_FAIL_ = Y_WIDTH <= 1;
+wire _TECHMAP_FAIL_ = A_WIDTH <= 8 && B_WIDTH <= 8;
 
-localparam WIDTH = ((Y_WIDTH + 3) / 4) * 4;
+localparam WIDTH = ((Y_WIDTH + 7) / 8) * 8;
 
 wire [Y_WIDTH-1:0] A_buf, B_buf;
 \$pos #(.A_SIGNED(A_SIGNED), .A_WIDTH(A_WIDTH), .Y_WIDTH(Y_WIDTH)) A_conv (.A(A), .Y(A_buf));
@@ -68,20 +70,19 @@ wire [WIDTH-1:0] BB = B_buf;
 wire [WIDTH-1:0] YY;
 wire [WIDTH:0] C;
 
-assign C[0] = 1;
+assign C[0] = 0;
 
 genvar i;
-generate for (i = 0; i < WIDTH; i = i + 4) begin:slice
-    \74AC283_1x1ADD4 adder_i (
-        .A(AA[i+3:i]),
-        .B(~BB[i+3:i]),
-        .CI(C[i]),
-        .S(YY[i+3:i]),
-        .CO(C[i+4]),
+generate for (i = 0; i < WIDTH; i = i + 8) begin:slice
+    \74HC688_1x1EQ8 eq_i (
+        .A(AA[i+7:i]),
+        .B(BB[i+7:i]),
+        .E(C[i]),
+        .Q(C[i+8])
     );
 end
 endgenerate
 
-assign Y = YY[Y_WIDTH-1:0];
+assign Y = C[WIDTH];
 
 endmodule
