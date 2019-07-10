48467a7adbc73b45b0fc29ee4a11888d42f70bf3
Pepijn de Vos <pepijndevos@gmail.com>
2019-07-10 18:07:27 +0200

    add forgotten file

---
+++ 74_shreg.v
@@ -0,0 +1,18 @@
+
+(* techmap_celltype = "$__SHREG_DFF_P_" *)
+module _80_74AC164(input C, D, output Q);
+parameter DEPTH = 1;
+
+wire [7:0] QBUF;
+wire [1:0] DS;
+
+assign DS = {1'b1, D};
+assign Q = QBUF[DEPTH-1];
+
+\74AC164_1x1SHREG8 _TECHMAP_REPLACE_ (
+    .DS(DS),
+    .MRn(1'b1),
+    .CP(C),
+    .Q(Q),
+);
+endmodule
