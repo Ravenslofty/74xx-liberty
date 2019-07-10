8db684cfa317bf1c49edba1b3ed2cdcd507ea183
Pepijn de Vos <pepijndevos@gmail.com>
2019-07-10 21:17:47 +0200

    add shreg enable and remove dffe unmap

--- 74_shreg.v
+++ 74_shreg.v
@@ -16,3 +16,21 @@ assign Q = QBUF[DEPTH-1];
     .Q(Q),
 );
 endmodule
+
+(* techmap_celltype = "$__SHREG_DFFE_PP_" *)
+module _80_74AC164E(input C, D, E, output Q);
+parameter DEPTH = 1;
+
+wire [7:0] QBUF;
+wire [1:0] DS;
+
+assign DS = {D, E};
+assign Q = QBUF[DEPTH-1];
+
+\74AC164_1x1SHREG8 _TECHMAP_REPLACE_ (
+    .DS(DS),
+    .MRn(1'b1),
+    .CP(C),
+    .Q(Q),
+);
+endmodule
